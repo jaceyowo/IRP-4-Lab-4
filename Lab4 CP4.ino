16cc83ecf93ee20aa225e72f278d8f8aa93b70bf
jaceyowo <117389772+jaceyowo@users.noreply.github.com>
2022-11-08 10:50:18 +0800

    Update Lab4 CP4.ino

--- Lab4 CP4.ino
+++ Lab4 CP4.ino
@@ -1,5 +1,5 @@
 /****************************************
- * Activity4: IoT
+   Activity4: IoT
  ****************************************/
 #include <WiFi.h>
 #include <PubSubClient.h>
@@ -19,14 +19,14 @@ float sensor;
 
 
 /****************************************
- * WIFI and Ubidots Credential
+   WIFI and Ubidots Credential
  ****************************************/
 #define WIFISSID "LAPTOP-GCAD0KR7 1308"                                        // Line1: Wifi SSID
 #define PASSWORD "0$o6U800"                                        // Line2: Wifi password
-#define TOKEN "BBFF-T1JBNrMvqah4idOlE2ZoEiqCC5vgfM"           		      // Line3: Ubidots TOKEN
+#define TOKEN "BBFF-T1JBNrMvqah4idOlE2ZoEiqCC5vgfM"                     // Line3: Ubidots TOKEN
 
 /****************************************
- * Define Constants and assign Pins
+   Define Constants and assign Pins
  ****************************************/
 #define MQTT_CLIENT_NAME "esp32irp4"                          // Line4: Set unique device client Name; 8-12 alphanumeric string 
 #define VARIABLE_LABEL "volts"                                // Line5: Set variable label; lowercase only
@@ -37,13 +37,13 @@ float sensor;
 #define PB 25                                                 // Line9: Assign Pushbutton to pin25
 #define RELAY 27                                              // Line10: Assign Relay control to pin27
 
-float Threshold = 3;                                          // Line11: Set Threshold intensity(in Volts)
-unsigned long interval= 10000;                                // Line12: Set interval between publish(in ms)
+float Threshold = 3.1;                                          // Line11: Set Threshold intensity(in Volts)
+unsigned long interval = 10000;                               // Line12: Set interval between publish(in ms)
 
 /************ Main Functions*************/
 void setup() {
   Serial.begin(115200);
-  pinMode(Builtin_LED, OUTPUT);   
+  pinMode(Builtin_LED, OUTPUT);
   pinMode(SENSOR, INPUT);                                     // Line13: Set SENSOR as INPUT
   pinMode(LED, OUTPUT);                                       // Line14: Set LED as OUTPUT
   pinMode(PB, INPUT);                                         // Line15: Set PB as INPUT
@@ -52,43 +52,43 @@ void setup() {
   digitalWrite(RELAY, HIGH);                                  // initialise RELAY pin HIGH; relay not energised
   digitalWrite(LED, HIGH);                                    // initialise LED pin HIGH; LED off
   iot_setup();                                                // Line17: Setup wifi and mqtt broker
-} 
+}
 
 void loop() {
-  if (millis() > timer){                                          // if uC clock exceed timer, read sensor and publish
-  if (!client.connected()) {                                // Line18: Check connection to broker
-    mqttconnect();                                          // Line19: Connect to broker
-    client.subscribe(topic_sub);                            // Line20: Subscribe to "topic_sub"
-  }                                                         // Line21:
-    sensor = analogRead(SENSOR)+ 150 ;                            // Read Va binary code; compensate ESP32 ADC offset error, ~150
-    Serial.print("Sense binary code:");                       
-    Serial.print(sensor);                                     
-  
-    sensor = sensor * 0.0008;                                     // Convert binary to voltage by x3.3/4095 ( = x0.0008)
-    Serial.print(" / Sense Voltage Va (V):");                 
-    Serial.print(sensor);                                     
+  if (millis() > timer) {                                         // if uC clock exceed timer, read sensor and publish
+    if (!client.connected()) {                                // Line18: Check connection to broker
+      mqttconnect();                                          // Line19: Connect to broker
+      client.subscribe(topic_sub);                            // Line20: Subscribe to "topic_sub"
+    }                                                         // Line21:
+    sensor = analogRead(SENSOR) + 150 ;                           // Read Va binary code; compensate ESP32 ADC offset error, ~150
+    Serial.print("Sense binary code:");
+    Serial.print(sensor);
 
-    if (sensor > Threshold){                                  // Line22: If sensor > Threshold, do line23,24
+    sensor = sensor * 0.0008;                                     // Convert binary to voltage by x3.3/4095 ( = x0.0008)
+    Serial.print(" / Sense Voltage Va (V):");
+    Serial.print(sensor);
+
+    if (sensor > Threshold) {                                 // Line22: If sensor > Threshold, do line23,24
       Serial.println("  / light is very dim");                // Line23: print text to serial monitor and end line
-                                                              // Line24-1: blank; add instruction if required
-                                                              // Line24-2:blank; add instruction if required
+      // Line24-1: blank; add instruction if required
+      // Line24-2:blank; add instruction if required
     }
-    else {                                                    // Line25: else, do line 26,27                                                    
+    else {                                                    // Line25: else, do line 26,27
       Serial.println("");                                     // Line26: print end line
-                                                              // Line27: blank; add instruction if required
+      // Line27: blank; add instruction if required
     }
-                                                              // Line28: blank; add Convert sensor voltage to sensor value if required
-  convert_json();                                               // convert payload to json string format
-  Serial.println("Publishing data to Ubidots");             // Line29: Print statement
-  client.publish(topic_pub, payload);                       // Line30: publish payload to topic_pub
-  
+    // Line28: blank; add Convert sensor voltage to sensor value if required
+    convert_json();                                               // convert payload to json string format
+    Serial.println("Publishing data to Ubidots");             // Line29: Print statement
+    client.publish(topic_pub, payload);                       // Line30: publish payload to topic_pub
+
     set_timer();                                                  // increment timer with preset interval
   }
   client.loop();                                            // Line31: mqtt real time function
 }
 
 /****************************************
- * Functions
+   Functions
  ****************************************/
 void callback(char* topic, byte* payload, unsigned int length) {
   char p[length + 1];
@@ -100,14 +100,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(topic);
   if (sub_payload < Threshold) {                              // Line32: if received subscribe payload < Threshold, do line 33
     digitalWrite(RELAY, LOW);                           // Line33: turn on Builtin_LED
-  } else{
+  } else {
     digitalWrite(RELAY, HIGH);                          // Line34: else turn off Builtin LED
   }
 }
- 
+
 /* Connect to MQTT broker */
 void mqttconnect() {
-    while (!client.connected()) {
+  while (!client.connected()) {
     Serial.println("Attempting MQTT connection...");
     if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
       Serial.println("Connected");
@@ -117,30 +117,30 @@ void mqttconnect() {
       Serial.println(" try again in 2 seconds");
       delay(2000);
     }
-   }
+  }
 }
 
 /* Brink builtin_LED twice */
-void blink_LED(){
-  digitalWrite(Builtin_LED, LOW);                                        
+void blink_LED() {
+  digitalWrite(Builtin_LED, LOW);
   delay(500);
   digitalWrite(Builtin_LED, HIGH);
   delay(500);
-  digitalWrite(Builtin_LED, LOW);  
+  digitalWrite(Builtin_LED, LOW);
   delay(500);
   digitalWrite(Builtin_LED, HIGH);
   delay(1000);
 }
 
 /* setup WIFI, MQTT, topics and subscribe callback function */
-void iot_setup(){ 
+void iot_setup() {
   WiFi.begin(WIFISSID, PASSWORD);
-  digitalWrite(Builtin_LED, HIGH); 
+  digitalWrite(Builtin_LED, HIGH);
   Serial.print("Wait for WiFi...");
   timer = millis() + 3000;
 
   while (WiFi.status() != WL_CONNECTED) {
-    if (millis() > timer){
+    if (millis() > timer) {
       ESP.restart();
     }
     Serial.print(".");
@@ -148,26 +148,27 @@ void iot_setup(){
   }
   Serial.println("WiFi Connected");
   blink_LED();
-  
+
   sprintf(topic_pub, "%s%s", "/v1.6/devices/", DEVICE_LABEL);                       // publish topic /v1.6/devices/'DEVICE_LABEL'
-  sprintf(topic_sub, "%s%s/%s/lv", "/v1.6/devices/", DEVICE_LABEL,VARIABLE_LABEL);  // subscribe topic /v1.6/devices/'DEVICE_LABEL'/'VARIABLE_LABEL'/lv  
+  sprintf(topic_sub, "%s%s/%s/lv", "/v1.6/devices/", DEVICE_LABEL, VARIABLE_LABEL); // subscribe topic /v1.6/devices/'DEVICE_LABEL'/'VARIABLE_LABEL'/lv
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   client.setServer(mqttBroker, 1883);
-  client.setCallback(callback); 
+  client.setCallback(callback);
 }
 
 /* setup payload in json format */
-void convert_json(){
-  dtostrf(sensor, 4, 2, str_sensor);                            
-  sprintf(payload, "%s", "");                                   
-  sprintf(payload, "{\"%s\":{\"value\": %s}}", VARIABLE_LABEL,str_sensor );   
+void convert_json() {
+  dtostrf(sensor, 4, 2, str_sensor);
+  sprintf(payload, "%s", "");
+  sprintf(payload, "{\"%s\":{\"value\": %s}}", VARIABLE_LABEL, str_sensor );
 }
 
 /* set timer for next publish base on preset interval, minimum 10sec */
-void set_timer(){
-  if (interval < 9999){
+void set_timer() {
+  if (interval < 9999) {
     interval = 10000;
   }
   timer = millis() + interval;
 }
+
